a53db2f1458014e070ad8064a4f6b3c1591a5a3b
AleksandrHoff <67598918+AleksandrHoff@users.noreply.github.com>
2020-07-10 14:50:43 +0300

    Homework 1.4

--- main.swift
+++ main.swift
@@ -1,162 +1,115 @@
 //
 //  main.swift
+//  Homework 1.4
+//
+//  Created by Александр Хофф on 10.07.2020.
+//
+
+import Foundation
+
+//
+//  main.swift
 //  Homework 1.3
 //
 //  Created by Александр Хофф on 06.07.2020.
 //
-
-//1. Описать несколько структур – любой легковой автомобиль и любой грузовик.
-//2. Структуры должны содержать марку авто, год выпуска, объем багажника/кузова, запущен ли двигатель, открыты ли окна, заполненный объем багажника.
-//3. Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
-//4. Добавить в структуры метод с одним аргументом типа перечисления, который будет менять свойства структуры в зависимости от действия.
-//5. Инициализировать несколько экземпляров структур. Применить к ним различные действия.
+//1. Описать класс Car c общими свойствами автомобилей и пустым методом действия по аналогии с прошлым заданием.
+//2. Описать пару его наследников trunkCar и sportСar. Подумать, какими отличительными свойствами обладают эти автомобили. Описать в каждом наследнике специфичные для него свойства.
+//3. Взять из прошлого урока enum с действиями над автомобилем. Подумать, какие особенные действия имеет trunkCar, а какие – sportCar. Добавить эти действия в перечисление.
+//4. В каждом подклассе переопределить метод действия с автомобилем в соответствии с его классом.
+//5. Создать несколько объектов каждого класса. Применить к ним различные действия.
 //6. Вывести значения свойств экземпляров в консоль.
 
 import Foundation
 
 enum Action {
+    case trailerOn;
+    case trailerOff
     case engineOn
-    case openWindows
-    case closeWindows
     case engineOff
-    case cargoLoading
-    case cargoUnloading
+    case turbineOn
+    case turbineOff
 }
-
-struct TruckCar {
-    let model: String
+class Car {
+    let color: String
     let year: Int
-    let engineWork: Bool
-    let openWindows: Bool
-    let vVolume: Double
-    let fillTheVolume: Double
-    let trailer: Bool
-    mutating func carAction (action: Action) {
-        switch action {
-        case .engineOn:
-            if engineWork == true {
-                print("Двигатель работает")
-            }
-            else {
-                print("Двигатель не работает")
-            }
-        case .openWindows:
-            if openWindows == true {
-                print("Окна открыты")
-            }
-            else {
-                print("Окна закрыты")
-            }
-            
-        case .engineOff:
-            if engineWork == true {
-                print("Двигатель не работает")
-            }
-            else {
-                print("Двигатель работает")
-            }
-        case .closeWindows:
-            if openWindows == true {
-                print("Окна закрыты")
-            }
-            else {
-                print("Окна открыты")
-            }
-        case .cargoLoading(weight:  ):
-            if fillTheVolume < vVolume {
-                print("Нельзя загрузить")
-            }
-            else {
-                print("груз загружен")
-            }
-        case .cargoUnloading(weight: ):
-            if vVolume < fillTheVolume {
-                print("Грузы выгружен")
-            }
-            else {
-                print("груз не выгружен")
-            }
-            if trailer == true {
-                print("Имеет  прицеп")
-            }
-            else {
-                print("не имеет прицепа")
-            }
-        }
-        
-    }
-    
-    init(model: String, year: Int, engineWork: Bool, openWindows: Bool, vVolume: Double, fillTheVolume: Double, trailer: Bool) {
-        self.model = model
+    let model: String
+    var vin: String
+    init(color: String, year: Int, model: String, vin: String) {
+        self.color = color
         self.year = year
-        self.engineWork = engineWork
-        self.openWindows = openWindows
-        self.vVolume = 20000
-        self.fillTheVolume = 10000
-        self.trailer = trailer
-    }
-    func resultTruckCar () {
-        print("марка автомобиля \(model), год выпуска \(year), двигатель \(engineWork), окна \(openWindows), объем кузова \(vVolume), загрузка кузова \(fillTheVolume), прицеп \(trailer)")
+        self.model = model
+        self.vin = vin
+        mutating func carAction (action: Action) {
+            switch Action.self {
+            case .engineOn:
+                if engineOn == true {
+                    print("Двигатель работает")
+                }
+                else {
+                    print("Двигатель не работает")
+                }
+            }
+        }
     }
 }
 
-struct Car {
-    let model: String
-    let year: Int
-    let engineWork: Bool
-    let openWindows: Bool
-    let vVolume: Double
-    let fillTheVolume: Double
-    mutating func carAction (action: Action) {
-        switch action {
-        case .engineOn:
-            if engineWork == true {
-                print("Двигатель работает")
-            }
-            else {
-                print("Двигатель не работает")
-            }
-        case .openWindows:
-            if openWindows == true {
-                print("Окна открыты")
-            }
-            else {
-                print("Окна закрыты")
-            }
-            
-        case .engineOff:
-            if engineWork == true {
-                print("Двигатель не работает")
-            }
-            else {
-                print("Двигатель работает")
-            }
-        case .closeWindows:
-            if openWindows == true {
-                print("Окна закрыты")
-            }
-            else {
-                print("Окна открыты")
-            }
-        case .cargoLoading(weight:  ):
-            if fillTheVolume < vVolume {
-                print("Нельзя загрузить")
-            }
-            else {
-                print("груз загружен")
-            }
-        case .cargoUnloading(weight: ):
-            if vVolume < fillTheVolume {
-                print("Грузы выгружен")
-            }
-            else {
-                print("груз не выгружен")
-            }
-            
-        }
+class TrunkCar: Car {
+    var trailer: Car      // Новое свойство
+    init(color: String, year: Int, model: String, vin: String, trailer: TrunkCar) {
+        self.trailer = trailer    // инициализируем новое свойство
+        // используем конструктор из родителя, чтобы завершить инициализацию класса
+        super.init(color: String, year: Int, model: String, vin: String)
     }
     
-    init(model: String, year: Int, engineWork: Bool, openWindows: Bool, vVolume: Double, fillTheVolume: Double, trailer: Bool) {
+    func hitchedTrailer() {                  // Новый метод
+        trailer = .hitched
+    }
+    
+    func notHitchedTrailer() {                // Новый метод
+        trailer = .notHitched
+    }
+    func TrunkCarAction (action: Action) {
+        switch action {
+        case .trailerOn:
+            if trailerOn == true {
+                print("Прицеп зацеплен")
+            }
+            else {
+                print("Прицеа отцеплен")
+            }
+        }
+    }
+}
+        
+        class SportCar: Car {
+            var turbine: Car      // Новое свойство
+            init(color: String, year: Int, model: String, vin: String, turbine: SportCar) {
+                self.turbine = turbine    // инициализируем новое свойство
+                // используем конструктор из родителя, чтобы завершить инициализацию класса
+                super.init(color: String, year: Int, model: String, vin: String)
+            }
+            
+            func turbineOn() {                  // Новый метод
+                turbine = .turbineOn
+            }
+            
+            func turbineOff() {                // Новый метод
+                turbine = .turbineOff
+            }
+            func carAction (action: Action) {
+                switch action {
+                case .turbineOn:
+                    if turbineOn == true {
+                        print("Турбина активна")
+                    }
+                    else {
+                        print("Турбина не активна")
+                    }
+                }
+            }
+        }
+init(color: String, year: Int, model: String, vin: String, turbine: SportCar) {
         self.model = model
         self.year = year
         self.engineWork = engineWork
@@ -164,9 +117,18 @@ struct Car {
         self.vVolume = 2000
         self.fillTheVolume = 1000
     }
-    
+
     func resultCar () {
         print("марка автомобиля \(model), год выпуска \(year), двигатель \(engineWork), окна \(openWindows), объем кузова \(vVolume), загрузка кузова \(fillTheVolume)")
     }
 }
+
+
+
+
+
+
+
+
+
 
